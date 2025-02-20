1c1e74ea065581ab70b38bf3008475c77dc7a808
CheolHwan2000 <olhw0816@gb-soft.co.kr>
2025-02-20 10:15:12 +0900

    Feat(#3): CalculateAverage 구현

--- app/src/main/java/com/example/ndkpr/MainActivity.kt
+++ app/src/main/java/com/example/ndkpr/MainActivity.kt
@@ -15,15 +15,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        val numbers = intArrayOf(10, 20, 30, 40, 50)
+        val avg = calculateAverage(numbers)
+        println("평균: $avg")  // 예상 출력: 30.0
     }
 
     /**
      * A native method that is implemented by the 'ndkpr' native library,
      * which is packaged with this application.
      */
-    external fun stringFromJNI(): String
+    external fun calculateAverage(numers: IntArray): Double
 
     companion object {
         // Used to load the 'ndkpr' library on application startup.
